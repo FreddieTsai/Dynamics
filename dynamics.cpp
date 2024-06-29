79dfbba23b6b0af2c9c13623ef416e8bbb231f1b
FreddieTsai <e14126325@gs.ncku.edu.tw>
2024-06-29 23:08:28 +0800

    新增運算角度的function，可以輸出位置了
    新增函式名calculateDegree

--- dynamics.cpp
+++ dynamics.cpp
@@ -5,26 +5,37 @@
 
 double angSpeed (double length, double degree);
 double angAcceler (double length, double degree);
+double calculateDegree (double length, double degree);
 
 using namespace std;
 
 int main() {
     const double pi = 3.1415926535;
-    double mass = 0, length = 0, /*initialDegree = 0*/ terminalDegree = 0, AngSpeed = 0, degree = 0, AngAcceler = 0;
+    double mass = 0, length = 0,  terminalDegree = 0, AngSpeed = 0, degree = 0, AngAcceler = 0;
     double location = 0;
     cout << "Please enter the mass." << endl;
     cin >> mass;
     cout << "Please enter the length." << endl;
     cin >> length;
-    cout << "Please enter the terminal degree";
+    cout << "Please enter the terminal degree" << endl;
     cin >> terminalDegree; 
     terminalDegree = (terminalDegree / 180) * pi;
+    ////星號中間這行是把得到的位置cout
+    //********* 
+    while( degree <= terminalDegree){
+        degree +=  calculateDegree( length, degree);
+        if( degree < (pi) / 2){
+            cout << degree << endl;
+        }
+    }
+    if(degree != terminalDegree){
+        cout << terminalDegree << endl;
+    }
+    //********
+    //degree =  calculateDegree( length, degree);
+    //cout << degree << endl;
     //degree = initialDegree;
     //t = 0.01, t是時間
-    while( degree < terminalDegree){
-        degree = angSpeed(length, degree) * 0.01 + 0.5 * angAcceler(length, degree) *0.0001;
-    }
-    //問題 : degree 代表著不同的位置，每試驗因為結束位置不一樣，要怎麼樣有一個伸縮自如的儲存空間
     return 0;
 }
 
@@ -40,3 +51,10 @@ double angAcceler (double length, double degree){
     AngAcceler = 1.5 * 9.81 * cos(degree) / length;
     return AngAcceler;
 }
+
+double calculateDegree (double length, double degree){
+    double t = 0.0001;
+    degree = angSpeed( length, degree ) * t + 0.5 * angAcceler(length, degree) * t * t;
+    return degree;
+}
+
