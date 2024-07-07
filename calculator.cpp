766de7862f4ed201461abede85035ffa6e6f8c2f
FreddieTsai <e14126325@gs.ncku.edu.tw>
2024-07-07 14:59:20 +0800

    calculator.cpp, calculator.h modification

--- calculator.cpp
+++ calculator.cpp
@@ -1,21 +1,31 @@
 #include <iostream>
-#include "calculator.h"
+#include <math.h>
 
+//length: 棍子的長度
+//degree: 棍子跟x軸正向夾角
 double angSpeed (double length, double degree){ 
     double squareOfAngSpeed,  AngSpeed;
-    squareOfAngSpeed = 3 * 9.81 * sin(degree) / length;
-    AngSpeed = pow(squareOfAngSpeed, 0.5); 
+    squareOfAngSpeed = 3 * 9.81 * sin(degree) / length; // 先算角速度的平方
+    AngSpeed = pow(squareOfAngSpeed, 0.5); // 算出角速度值
     return AngSpeed;
 }
 
+//length: 棍子的長度
+//degree: 棍子跟x軸正向夾角
 double angAcceler (double length, double degree){
     double AngAcceler;
-    AngAcceler = 1.5 * 9.81 * cos(degree) / length;
+    AngAcceler = 1.5 * 9.81 * cos(degree) / length;  // 算角加速度
     return AngAcceler;
 }
 
+//length: 棍子的長度
+//degree: 棍子跟x軸正向夾角
 double calculateDegree (double length, double degree){
-    double t = 0.0001;
+    
+    double t = 0.0001;  
+    // t 為單位時間
+    // 設定單位時間 t = 0.0001
+
     degree = angSpeed(length, degree ) * t + 0.5 * angAcceler(length, degree) * t * t;
     return degree;
 }
--- calculator.h
+++ calculator.h
@@ -1,11 +1,15 @@
 #ifndef CALCULATOR_H
 #define CALCULATOR_H
-#include "calculator.h"
-//#include<iostream>
 #endif
 
-//double mass = 0, length = 0,  terminalDegree = 0, AngSpeed = 0, degree = 0, AngAcceler = 0, location = 0;
+//length: 棍子的長度
+//degree: 棍子跟x軸正向夾角
 
-double angSpeed (double length, double degree);
-double angAcceler (double length, double degree);
+double angSpeed (double length, double degree);  
+//運算角速度，回傳角速度給calculateDegree
+
+double angAcceler (double length, double degree); 
+//運算角加速度，回傳角加速度給calculateDegree
+
 double calculateDegree (double length, double degree);
+//運算棍落下的每個單位時間的棍子與x軸正向夾角回傳給main
