0ade9d9e9f645123ac2aefc3505d7ccd01ac0ec8
FreddieTsai <e14126325@gs.ncku.edu.tw>
2024-07-07 15:04:23 +0800

    Update calculator.cpp

--- calculator.cpp
+++ calculator.cpp
@@ -1,26 +1,29 @@
 #include <iostream>
 #include <math.h>
 
-//length: 棍子的長度
-//degree: 棍子跟x軸正向夾角
+
 double angSpeed (double length, double degree){ 
+    //length: 棍子的長度
+    //degree: 棍子跟x軸正向夾角
     double squareOfAngSpeed,  AngSpeed;
     squareOfAngSpeed = 3 * 9.81 * sin(degree) / length; // 先算角速度的平方
     AngSpeed = pow(squareOfAngSpeed, 0.5); // 算出角速度值
     return AngSpeed;
 }
 
-//length: 棍子的長度
-//degree: 棍子跟x軸正向夾角
+
 double angAcceler (double length, double degree){
+    //length: 棍子的長度
+    //degree: 棍子跟x軸正向夾角
     double AngAcceler;
     AngAcceler = 1.5 * 9.81 * cos(degree) / length;  // 算角加速度
     return AngAcceler;
 }
 
-//length: 棍子的長度
-//degree: 棍子跟x軸正向夾角
+
 double calculateDegree (double length, double degree){
+    //length: 棍子的長度
+    //degree: 棍子跟x軸正向夾角
     
     double t = 0.0001;  
     // t 為單位時間
